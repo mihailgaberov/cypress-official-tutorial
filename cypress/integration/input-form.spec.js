6e5e82c4ec3745ddb54151cf2f18a0468503f608
Mihail Gaberov <mihail.gaberov@sequel.com>
2021-07-16 10:35:45 +0200

    Initial commit

---
+++ cypress/integration/input-form.spec.js
@@ -0,0 +1,14 @@
+describe('Input form', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3030')
+  })
+
+  it('focuses input on load', () => {
+    cy.focused().should('have.class', 'new-todo')
+  })
+
+  it('accepts input', () => {
+    const typedText = 'Buy Milk'
+    cy.get('.new-todo').type(typedText).should('have.value', typedText)
+  })
+})
